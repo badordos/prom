f5e8e528f0a39d54f653dfc580bbd2f0207c0197
Vladisalav Yarlykov <badordos@gmail.com>
2020-03-27 12:50:59 +0500

    required fields contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <!-- Contact section   -->
+    @include('components.errors')
     <section class="contact-section spad">
         <div class="container">
             <div class="row">
@@ -35,16 +36,16 @@
                         @csrf
                         <div class="row">
                             <div class="col-lg-6">
-                                <input type="text" name="name" placeholder="Ваше имя">
+                                <input type="text" name="name" placeholder="Ваше имя" required>
                             </div>
                             <div class="col-lg-6">
-                                <input type="email" name="email" placeholder="Ваша почта">
+                                <input type="email" name="email" placeholder="Ваша почта" required>
                             </div>
                             <div class="col-lg-4">
                             </div>
                             <div class="col-lg-12">
-                                <input type="text" name="subject" placeholder="Заголовок">
-                                <textarea class="text-msg" placeholder="Сообщение" name="message"></textarea>
+                                <input type="text" name="subject" placeholder="Заголовок" required>
+                                <textarea class="text-msg" placeholder="Сообщение" name="message" required></textarea>
                                 {!! app('captcha')->display() !!}
                                 <button class="site-btn" type="submit">Отправить</button>
                             </div>
