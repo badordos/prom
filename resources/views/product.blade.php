09a447dca812876106d898172c478d2c0d5b23c1
badordos <badordos@gmail.com>
2021-01-09 00:47:30 +0500

    img product fix

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -15,7 +15,10 @@
             <div class="col-lg-8 col-md-6 col-sm-12">
                 <div class="blog-post">
                     <h1>{{$product->title}}</h1>
-                    <div class="blog-thumb set-bg" data-setbg="{{$image = $product->getFirstMedia('image') ? $image->getUrl() : '/img/about.jpg'}}">
+                    @php
+                        $image = $product->getFirstMedia('image')
+                    @endphp
+                    <div class="blog-thumb set-bg" data-setbg="{{$image ? $image->getUrl() : '/img/about.jpg'}}">
                     </div>
                     <h2>{{$product->short_desc}}</h2>
                     <p>{{$product->desc}}</p>
